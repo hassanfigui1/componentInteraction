cedf4eaf523e64eae26ba3eda9e0bff06b298aee
hassan figuigui <hassan.figuigui7@aiesec.net>
2023-03-31 11:02:07 +0000

    adding navbar

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -8,7 +8,10 @@ import { User } from '../classes/user';
 })
 export class UsersComponent {
   user = new User(1,"Hassan Figuigui",25);
-  users: User[] = [];
-
+  user1 = new User(1,"Hassan Figuigui",25);
+  user2 = new User(1,"Hassan Figuigui",25);
+  user3 = new User(1,"Hassan Figuigui",25);
+  user4 = new User(1,"Hassan Figuigui",25);
+  users: User[] = [new User(1,"Hassan Figuigui",25),new User(2,"Malika Figuigui",24)];
 
 }
