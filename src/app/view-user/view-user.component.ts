9e9e04409f04d3ae55a0a25f940b50025807f725
hassan figuigui <hassan.figuigui7@aiesec.net>
2023-04-02 04:18:57 +0000

    viewing a user using its id

--- src/app/view-user/view-user.component.ts
+++ src/app/view-user/view-user.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { UserInfoService } from '../services/user-info.service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-view-user',
@@ -8,9 +10,23 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ViewUserComponent {
   user_id:any;
-  constructor(private activatedRoute : ActivatedRoute){}
+  user:any;
+  constructor(private activatedRoute: ActivatedRoute, private userService: UserInfoService){}
+  getUserById(id:any) {
+      this.user = this.userService.users.find((obj:any) => {
+        return obj.getId === id;
+      });
+  }
   ngOnInit() : void{
     this.user_id = this.activatedRoute.snapshot.paramMap.get('id');
-    console.log("heloooooooooooo :         ",this.user_id);
+    console.log(typeof(this.user_id));
+    this.user_id = Number(this.user_id);
+    console.log(typeof(this.user_id));
+    this.user = this.userService.users.find((obj) => {
+      return obj.getId === this.user_id;
+    });    
+    console.log(this.user);
+    // this.getUserById(this.user_id);
+    // console.log("heloooooooooooo :         ",this.user_id);
   }
 }
