ada1ed52bac819cc4b486c273d200735bb4eef17
hassan figuigui <hassan.figuigui7@aiesec.net>
2023-04-02 11:56:45 +0000

    error showed up again

--- src/app/edit-user/edit-user.component.ts
+++ src/app/edit-user/edit-user.component.ts
@@ -10,6 +10,8 @@ import { UserInfoService } from '../services/user-info.service';
 export class EditUserComponent {
   user_id:any;
   user:any;
+  user_fullname:any;
+  user_age:any;
   constructor(private activatedRoute: ActivatedRoute, private userService: UserInfoService){}
   ngOnInit() : void{
     this.user_id = this.activatedRoute.snapshot.paramMap.get('id');
@@ -17,6 +19,12 @@ export class EditUserComponent {
     this.user = this.userService.users.find((obj) => {
       return obj.getId === this.user_id;
     });    
+      this.user_fullname = this.user.getName();
+      this.user_age = this.user.getAge();
+  }
+
+  edit(){
+    return null;
   }
 
   
